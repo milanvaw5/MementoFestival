8fbb20aa3c2545b11e90731cd485ab92c51ee58b
Milan Vanalderwerelt <milan.vanalderwerelt@student.howest.be>
2021-01-17 16:42:30 +0100

    looking to collide all words with all words

--- js/classes/scenes/LBoxScene.js
+++ js/classes/scenes/LBoxScene.js
@@ -1,5 +1,8 @@
 //let letters;
 //let numberOfWords = 0;
+let j = 0;
+let word;
+let words= [];
 let backgroundimage;
 let spawnWordInterval;
 
@@ -27,7 +30,7 @@ export default class LetterBoxScene extends Phaser.Scene {
     console.log(`CREATE`);
     //this.physics.world.checkCollision.up = false;
 
-    /*let words = this.physics.add.group({
+    words = this.physics.add.group({
       key: 'block',
       frameQuantity: 6,
       bounceY: 0.5,
@@ -35,13 +38,13 @@ export default class LetterBoxScene extends Phaser.Scene {
       velocityY: 300,
       collideWorldBounds: true,
       setXY: { x: 400, y: 0, stepY: -200 }
-  });*/
+  });
 
     backgroundimage = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'backgroundimage');
 
     this.spawnWords();
       spawnWordInterval = this.time.addEvent({
-      delay: 10000,
+      delay: 1000,
       callback: this.spawnWords,
       callbackScope: this,
       args: [],
@@ -51,23 +54,40 @@ export default class LetterBoxScene extends Phaser.Scene {
   }
 
   update(){
-    //this.physics.arcade.collide(word, word);
+    /*for (var i = 0; i <= j; i++)
+    {
+      if ( i != j) {
+        this.physics.collide([j], [i]);
+        console.log('botst ' + [j] +'met '+[i] +'?');
+      }
+      else {
+        console.log('eigen blokje');
+      }
+    }*/
+    this.physics.collide(words, 1);
+    this.physics.collide(words, 2);
+    this.physics.collide(words, 3);
+    this.physics.collide(words, 4);
+    this.physics.collide(words, 5);
+    this.physics.collide(words, 6);
+    this.physics.collide(words, 7);
+    this.physics.collide(words, 8);
+    this.physics.collide(words, 9);
+    this.physics.collide(words, 10);
+    //game.physics.arcade.collide(sprite, sprite2);
   }
 
   spawnWords() {
-    const word = this.physics.add.sprite(Phaser.Math.Between(0, this.cameras.main.width / 2), 0, 'word');
-    word.body.setGravityY(300);
-    word.body.mass = 20;
-    word.body.acceleration.set(0, 0.8);
-    word.setCollideWorldBounds(true);
-    word.body.setBounce(0, 0.7);
-    //this.physics.add.collider(word, word, this.collideWords, null, this);
-    //words.add(word); //geeft error this.words undefined
-    word.setVelocityY(100);
-    console.log('SPAWN WORD');
+      word = this.physics.add.sprite(Phaser.Math.Between(0, this.cameras.main.width / 2), 0, j);
+      word.body.setGravityY(300);
+      word.body.mass = 20;
+      word.body.acceleration.set(0, 0.8);
+      word.setCollideWorldBounds(true);
+      word.body.setBounce(0, 0.7);
+      words.add(j);
+      word.setVelocityY(100);
+      console.log('SPAWN WORD '+[j]);
+      j++;
   }
 
-  //collideWords(word, word){
-
-  //}
 }
