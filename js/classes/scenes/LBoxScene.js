cf639417a2c31b6e695a308447911575911580ae
Milan Vanalderwerelt <milan.vanalderwerelt@student.howest.be>
2021-01-25 13:16:06 +0100

    posenet

--- js/classes/scenes/LBoxScene.js
+++ js/classes/scenes/LBoxScene.js
@@ -13,15 +13,129 @@ let auteurInput = ['Verloren','hinkel ik','over de sproeten op mijn vingers',
 'Onontdekt','en zorgeloos','dolend door mijn eigen hoofd'];
 
 const lettersprites = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+const videoWidth = 600;
+const videoHeight = 700;
+const colorRight = "red";
+const colorLeft = "black";
+
+const poseNetState = {
+  algorithm: 'single-pose',
+  input: {
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    inputResolution: 513,
+    multiplier: 0.75,
+    quantBytes: 2
+  },
+  singlePoseDetection: {
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+  },
+  output: {
+    showVideo: true,
+    showPoints: true,
+  },
+};
+
+const detectPoseInRealTime = (video) => {
+  const canvas = document.getElementById('output');
+  const ctx = canvas.getContext('2d');
+  const flipPoseHorizontal = true;
+  console.log(video)
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  console.log(videoHeight)
+  async function poseDetectionFrame() {
+    let poses = [];
+    let minPoseConfidence;
+    let minPartConfidence;
+
+    switch (poseNetState.algorithm) {
+      case 'single-pose':
+        const pose = await poseNetModel.estimatePoses(video, {
+          flipHorizontal: flipPoseHorizontal,
+          decodingMethod: 'single-person'
+        });
+        poses = poses.concat(pose);
+        minPoseConfidence = +poseNetState.singlePoseDetection.minPoseConfidence;
+        minPartConfidence = +poseNetState.singlePoseDetection.minPartConfidence;
+        break;
+    }
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    if (poseNetState.output.showVideo) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.translate(-videoWidth, 0);
+      ctx.restore();
+    }
+
+    poses.forEach(({score, keypoints}) => {
+      if (score >= minPoseConfidence) {
+        if (poseNetState.output.showPoints) {
+          drawKeypoints(keypoints, minPartConfidence, ctx);
+        }
+      }
+    });
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+const setupCamera = async () => {
+  const video = document.getElementById('video');
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      width: videoWidth,
+      height: videoHeight,
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => resolve(video);
+  });
+}
+
+const startVideo = async () => {
+
+  try {
+    video = await setupCamera();
+    video.play();
+
+  } catch (e) {
+    throw e;
+  }
+  detectPoseInRealTime(video);
+}
+
+
 export default class LetterBoxScene extends Phaser.Scene {
 
   constructor(config){
     super(config);
   }
 
-  init(){
+  init = async () => {
     //reset some variables
     console.log('INITLIALIZING');
+    poseNetModel = await posenet.load({
+      architecture: poseNetState.input.architecture,
+      outputStride: poseNetState.input.outputStride,
+      inputResolution: poseNetState.input.inputResolution,
+      multiplier: poseNetState.input.multiplier,
+      quantBytes: poseNetState.input.quantBytes
+  });
+  console.log(poseNetModel)
+  startVideo();
+
   }
 
   preload(){
@@ -121,5 +235,41 @@ export default class LetterBoxScene extends Phaser.Scene {
   letters.push(split);
 
   };
+
+
+  drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+    let leftWrist = keypoints.find(point => point.part === 'leftWrist');
+    let rightWrist = keypoints.find(point => point.part === 'rightWrist');
+    let leftEye = keypoints.find(point => point.part === 'leftEye');
+    let rightEye = keypoints.find(point => point.part === 'rightEye');
+    // plaats joints naar believen
+
+    if (leftWrist.score > minConfidence) {
+        const {y, x} = leftWrist.position;
+        drawPoint(ctx, y * scale, x * scale, 10, colorLeft);
+    }
+
+    if (rightWrist.score > minConfidence) {
+        const {y, x} = rightWrist.position;
+        drawPoint(ctx, y * scale, x * scale, 10, colorRight);
+    }
+
+    if (leftEye.score > minConfidence) {
+      const {y, x} = leftEye.position;
+      drawPoint(ctx, y * scale, x * scale, 10, colorLeft);
+  }
+
+  if (rightEye.score > minConfidence) {
+      const {y, x} = rightEye.position;
+      drawPoint(ctx, y * scale, x * scale, 10, colorRight);
+  }
+}
+
+drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
 
 }
