afd734741c3aa8dfe239c869a0ef6bbd02b33bd7
Stijn Hellem <stijn.hellem@student.howest.be>
2021-01-29 11:10:18 +0100

    added helpers with formHandler and randomVideo for visitor and modified some html in indexV.html for the form

---
+++ public/js/helpers/randomVideo.js
@@ -0,0 +1,27 @@
+{
+    let randomVideo = 0;
+    const videos = ["video0.mp4", "video2.mp4"];
+    const $video = document.querySelector(`.previewVideo`);
+
+    const handleEndedVideo = () => {
+        randomVideo++;
+        if(randomVideo >= videos.length){ // videos.length is 2
+            randomVideo = 0;
+        }
+        $video.src = `assets/video/${videos[randomVideo]}`;
+    }
+
+    const init = () => {
+        $video.addEventListener(`ended`, handleEndedVideo);
+        randomVideo = randomNumber();
+        $video.src = `assets/video/${videos[randomVideo]}`;
+    }
+
+
+    const randomNumber = () => {
+        const random = Math.floor(Math.random(1)*2);
+        return random;
+    }
+
+    init();
+}
