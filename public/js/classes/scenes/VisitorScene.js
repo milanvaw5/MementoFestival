ed75495689817b77041fa0d793db3ffeb9f9257c
Stijn Hellem <stijn.hellem@student.howest.be>
2021-02-10 14:11:55 +0100

    letter removal added

--- public/js/classes/scenes/VisitorScene.js
+++ public/js/classes/scenes/VisitorScene.js
@@ -31,6 +31,7 @@ let isClickable = false;
 let isHandClickable = true;
 let isHeartClickable = true;
 let isShakeClickable = true;
+let isClearing = false;
 
 const forbiddenWordsEn = ["4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"];
 const forbiddenWordsNl = ["abortus", "anaal", "anus", "ezel", "ass-neuker", "ezels", "lul","klootzakken","ballbag","ballen","bastaard-","bellend","beestachtig","beestachtigheid","teef","teven","zeuren","bloedig","pijpbeurt","bollok","lobbes","tieten","borsten","buceta","kont","mikpunt","tapijt muncher","spleet","cipa","clitoris","pik","cock-sucker","hanen","coon","onzin","cum","klaarkomen","cunillingus","kut","vloek","dick","dildo","dildo's","dink","dog-neuker","duche","dijk","ejaculaat","ejaculatie","ejaculeert","zaadlozing","flikker","fagging","takkenbossen","fanny","felching","fellatio","flens","neuken","fucked","neuker","fuckers","fuckings","neukt","fudge packer","god-damned","godverdomme","hel","hore","geile","jerk-off","kock","schaamlippen","lust","begeren","masochist","masturberen","moeder klootzak","nazi","neger","negers","orgasim","orgasme","orgasmes","snavel","penis","pissen","dronken","pisser","pissend","pissing","donder op","achterschip","porno","pornografie","prik","prikt","pube","kutjes","kutje","verkrachting","rapist","rectum","vertragen","rimmen","sadist","schroeven","scrotum","sperma","seks","shag","shagging","shemale","stront","shite","diarree","schijten","shitty","slet","sletten","smegma","roetvlek","rukken","son-of-a-teef","spac","fut","zaadbal","mees","titt","drol","vagina","viagra","vulva","wang","wank","hoer","x beoordeeld","xxx"];
@@ -354,6 +355,14 @@ export default class VisitorScene extends Phaser.Scene {
     //this.input.mouse.onMouseWheel.preventDefault = false
 
 
+    this.time.addEvent({
+      delay: 300,             
+      callback: this.clearLetters,
+      callbackScope : this,
+      args: [letters],
+      loop: true
+  });
+   
 
 
 
@@ -408,11 +417,50 @@ export default class VisitorScene extends Phaser.Scene {
     //timer = setTimeout(this.readInAuteurInput(), 50000);
   }
 
+  
+  clearLetters(){
+
+      if(letters.length > 10 && isClearing === false){
+        isClearing = true;
+        let letterWaitingToBeDestroyed = [];
+        for (let index = 0; index < 4; index++) {
+         
+          
+        
+        
+        const rand = Math.floor(Math.random()*letters.length);
+  
+        console.log(rand);
+        this.tweens.add({
+          targets: letters[rand],
+          scale: 0,
+          duration: 100,
+          ease: 'Linear'
+      });
+      
+      letterWaitingToBeDestroyed.push(letters[rand]);
+     // letters[rand].destroy();
+      letters.splice(rand, 1);
+      console.log('Dit is de index:' + index);
+      if(index === 3){
+        setTimeout(function(){
+          letterWaitingToBeDestroyed.forEach(letter => {
+            letter.destroy();
+          });
+        }, 300);
+        
+        isClearing = false;
+      }
+
+      }
+    }
+  }
+  
+
 
   resize(){
     console.log("is resized")
   }
-
 
   update(){
   
@@ -431,6 +479,8 @@ export default class VisitorScene extends Phaser.Scene {
       }
       console.log(letters);
     }
+
+   
     /*
     jointPositionsGebruikers.nosePos.x += ( jointPositionsGebruikersTarget.nosePosTarget.x - jointPositionsGebruikers.nosePos.x ) / 10;
     jointPositionsGebruikers.nosePos.y += ( jointPositionsGebruikersTarget.nosePosTarget.y - jointPositionsGebruikers.nosePos.y ) / 10;
@@ -646,6 +696,20 @@ handleLetterArrays(l){
         socket.emit('requestHearts');
         socket.emit('requestHighfives');
       });
+      socket.on('synchronize', () => {
+        letters.forEach(letter => {
+            letter.destroy();
+            this.matter.world.remove(letter);
+           
+        });
+        letters = [];
+        feelings.forEach(feeling => {
+          feeling.destroy();
+          this.matter.world.remove(feeling);
+          
+      });
+      feelings = [];
+      });
       socket.on('getWords', words => {
         words.forEach(word => {
           woordje = word;
@@ -690,10 +754,10 @@ handleLetterArrays(l){
       socket.on('shakeAll', () => {
 
         letters.forEach(letter => {
-          letter.setVelocity(20, 30)
+          letter.setVelocity(20, -30)
         });
         feelings.forEach(feeling => {
-          feeling.setVelocity(20, 30)
+          feeling.setVelocity(20, -30)
         });
 
       });
@@ -708,7 +772,7 @@ handleLetterArrays(l){
         $highFiveText.textContent = "Je hebt een highfive terug van uit Kortrijk!";
         $popupHighfive.style.animation = "appearAndDisappear 4s ease";
         $popupHighfive.style.opacity = "0";
-        $highFiveImg.style.animation = "highFive 1s ease";
+        $highFiveImg.style.animation = "highFive 4s ease";
         setTimeout(function(){
           $popupHighfive.style.animation = "none";
           $popupHighfive.style.opacity = "0";
@@ -738,6 +802,29 @@ handleLetterArrays(l){
 
 
       });
+/*
+      socket.on('clearLetterAll', letter => {
+        
+  
+      
+        this.tweens.add({
+          targets: letter,
+          scale: 0,
+          duration: 100,
+          ease: 'Linear'
+      });
+   
+      setTimeout(function(){
+
+        letter.destroy();
+        //this.matter.world.remove(letters[rand]);
+   
+        letters.splice(letters.indexOf(letter), 1);
+        console.log(letters);
+       }, 200);
+      
+      });
+      */
       socket.on('selectedFeeling', selectedFeeling => {
         this.createFeeling(selectedFeeling);
 
