542dba2a8041728dbf0084f2f84a24a47883df47
Milan Vanalderwerelt <milan.vanalderwerelt@student.howest.be>
2021-02-10 20:18:46 +0100

    number vis

--- public/js/classes/scenes/AdminScene.js
+++ public/js/classes/scenes/AdminScene.js
@@ -469,7 +469,7 @@ export default class AdminScene extends Phaser.Scene {
     //random gekozen om zo nieuwe woorden en mysterie te creëren
   clearLetters(){
 
-    if(letters.length > 90 && isClearing === false){
+    if(letters.length > 80 && isClearing === false){
       isClearing = true;
       let letterWaitingToBeDestroyed = [];
       for (let index = 0; index < 4; index++) {
